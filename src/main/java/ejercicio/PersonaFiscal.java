87a98b0cc6047a6fe700eb5c338666ac8ebba6a5
Abel <Abel@LAPTOP-F7VT8883>
2018-03-15 16:40:55 +0100

    2

--- src/main/java/ejercicio/PersonaFiscal.java
+++ src/main/java/ejercicio/PersonaFiscal.java
@@ -2,4 +2,35 @@ package ejercicio;
 
 public class PersonaFiscal {
 
+	String nombre;
+	int edad;
+	int sueldo;
+
+	public int calcularImpuesto() {
+		return -1;
+	}
+
+	public String getNombre() {
+		return this.nombre;
+	}
+
+	public void setNombre(String _nombre) {
+		this.nombre = _nombre;
+	}
+
+	public int getEdad() {
+		return this.edad;
+	}
+
+	public void setEdad(int _edad) {
+		this.edad = _edad;
+	}
+
+	public int getSueldo() {
+		return this.sueldo;
+	}
+
+	public void setSueldo(int _sueldo) {
+		this.sueldo = _sueldo;
+	}
 }
---
+++ src/main/java/ejercicio/TipoImpuesto.java
@@ -0,0 +1,32 @@
+package ejercicio;
+
+public class TipoImpuesto {
+
+	int porcentaje;
+	int porcentaje30;
+	String nombre;
+
+	public int getPorcentaje() {
+		return this.porcentaje;
+	}
+
+	public void setPorcentaje(int _porcentaje) {
+		this.porcentaje = _porcentaje;
+	}
+
+	public int getPorcentaje30() {
+		return this.porcentaje30;
+	}
+
+	public void setPorcentaje30(int _porcentaje30) {
+		this.porcentaje30 = _porcentaje30;
+	}
+
+	public String getNombre() {
+		return this.nombre;
+	}
+
+	public void setNombre(String _nombre) {
+		this.nombre = _nombre;
+	}
+}
